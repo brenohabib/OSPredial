7e5ad7fce676634fa9dc4ead784e608f925c84b3
brenohabib <brenohabib2013@gmail.com>
2024-09-13 19:52:38 -0300

    v0.2

--- src/main/java/org/atividadeJava/atividade3/Prova2e3/Menu/Register.java
+++ src/main/java/org/atividadeJava/atividade3/Prova2e3/Menu/Register.java
@@ -6,6 +6,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,32 +21,28 @@ public class Register extends JFrame {
     private JPanel backgroundPanel;
     private JPanel frontPanel;
     private JPanel residentRegistrationPanel;
-    private JTextField blockInput;
-    private JTextField apartmentInput;
     private JButton registerButton;
     private JTextField nameInput;
     private JTextField emailInput;
     private JPasswordField passwordInput;
-    private JTextField lobbyInput;
     private JPanel logo;
     private JLabel labelPortaria;
     private JLabel labelBloco;
     private JLabel labelApto;
+    private JButton hasAccountButton;
+    private JComboBox<String> lobbyCB;
+    private JComboBox<String> blockCB;
+    private JComboBox<String> apartmentCB;
 
     private void createUIComponents() {
         frontPanel = new RoundedPanel(30);
         nameInput = new RoundedTextField(15);
         emailInput = new RoundedTextField(15);
         passwordInput = new RoundedPasswordField(15);
-        lobbyInput = new RoundedTextField(10);
-        apartmentInput = new RoundedTextField(10);
-        blockInput = new RoundedTextField(10);
-
         frontPanel = new RoundedPanel(30);
-
+        registerButton = new RoundedButton();
         mainPanel = new ImagePanel("src/main/java/Caixa 2.png");
         logo = new ImagePanel("src/main/java/Logo.png");
-        registerButton = new RoundedButton();
     }
 
     public Register() throws HeadlessException {
@@ -55,6 +54,21 @@ public class Register extends JFrame {
         labelPortaria.setForeground(Color.white);
         labelBloco.setForeground(Color.white);
         labelApto.setForeground(Color.white);
+        hasAccountButton.setText("<html><u>já possui uma conta?</u></html>");
+
+        String[] lobbyOptions = new String[]{"Portaria Principal", "Portaria Oeste"};
+        for (String option : lobbyOptions) {
+            lobbyCB.addItem(option);
+        }
+        String[] blockOptions = new String[]{"1", "2", "3", "4", "5"};
+        for (String option : blockOptions) {
+            blockCB.addItem(option);
+        }
+        String[] apartmentOptions = new String[]{"101", "102", "103", "201", "202", "203", "301", "302", "303"};
+        for (String option : apartmentOptions) {
+            apartmentCB.addItem(option);
+        }
+
         pack();
         setVisible(true);
 
@@ -76,24 +90,36 @@ public class Register extends JFrame {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
-
-                if (nameInput.getText().isEmpty()) {
-                    JOptionPane.showMessageDialog(mainPanel, "Por favor, preencha todos os campos");
+                Matcher emailMatcher = emailPattern.matcher(emailInput.getText());
+                if (nameInput.getText().isEmpty() ||
+                        emailInput.getText().isEmpty() ||
+                        passwordInput.getPassword().length == 0) {
+                    JOptionPane.showMessageDialog(mainPanel, "Por favor, preencha todos os campos", "", JOptionPane.INFORMATION_MESSAGE);
                     return;
                 }
-
-                Matcher emailMatcher = emailPattern.matcher(emailInput.getText());
                 if (!emailMatcher.matches()) {
-                    JOptionPane.showMessageDialog(mainPanel, "Formato de e-mail inválido.");
+                    JOptionPane.showMessageDialog(mainPanel, "Formato de e-mail inválido.", "", JOptionPane.INFORMATION_MESSAGE);
                     return;
                 }
 
                 if (passwordInput.getPassword().length < 8) {
-                    JOptionPane.showMessageDialog(mainPanel, "A senha deve ter no mínimo 8 caracteres.");
+                    JOptionPane.showMessageDialog(mainPanel, "A senha deve ter no mínimo 8 caracteres.", "", JOptionPane.INFORMATION_MESSAGE);
                     return;
                 }
 
-                JOptionPane.showMessageDialog(mainPanel, "Registro realizado com sucesso!");
+                try {
+                    saveToCSV(
+                            nameInput.getText(),
+                            emailInput.getText(),
+                            new String(passwordInput.getPassword()),
+                            (String) lobbyCB.getSelectedItem(),
+                            (String )blockCB.getSelectedItem(),
+                            (String) apartmentCB.getSelectedItem()
+                    );
+                    JOptionPane.showMessageDialog(mainPanel, "Registro realizado com sucesso!");
+                } catch (IOException ex) {
+                    JOptionPane.showMessageDialog(mainPanel, "Erro ao salvar o registro.", "Erro", JOptionPane.ERROR_MESSAGE);
+                }
             }
 
             @Override
@@ -110,5 +136,15 @@ public class Register extends JFrame {
                 registerButton.setSize(200, 50);
             }
         });
+    }
+
+    private void saveToCSV(String name, String email, String password, String lobby, String apartment, String block) throws IOException {
+        String filePath = "src/main/java/org/atividadeJava/atividade3/Prova2e3/records.csv";
+
+        try (FileWriter fileWriter = new FileWriter(filePath, true);
+             PrintWriter printWriter = new PrintWriter(fileWriter)) {
+
+            printWriter.printf("%s,%s,%s,%s,%s,%s%n", name, email, password, lobby, apartment, block);
+        }
     }
 }
